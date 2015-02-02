d8e356027de8a2314b559544a4d7aff0b72a2ff0
Attila Dobi <dobiattila@gmail.com>
2015-02-02 10:24:15 +0200

    resume modifications

--- adobi.hu/views/resume/index.php
+++ adobi.hu/views/resume/index.php
@@ -3,59 +3,53 @@
 		
 		<div id="contact-info" class = "vcard">
 			
-			<h1 class = "fn">Attila Dobi</h1>
-			<!-- 
-			<p>
-				Cell: <span class = "cell-hu">+36-70-53-01-132</span> <br />
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>+40-74-02-98-580</span><br />
-				Email: <a href="mailto" class = "email">hello[at]adobi.hu</a><br />
-				Web:<a href="http://adobi.hu" class = "web">http://adobi.hu</a>
-			</p>
-			 -->
-			<dl>
-				<dd class="clear"></dd>
-				
-				<dt>Cell:</dt>
-				<dd class = "tel">+36-70-39-27-700</dd>
-				<dd class="clear"></dd>
-				<!--
-				<dt>&nbsp;</dt>
-				<dd class = "tel">+36-70-53-01-132</dd>
-				<dd class="clear"></dd>
-				-->
-				<dt>&nbsp;</dt>
-				<dd class = "tel">+40-74-02-98-580</dd>
-				<dd class="clear"></dd>
-
-				<dt>Email:</dt>
-				<dd><a href="mailto:hello@adobi.hu" class = "email">hello[at]adobi.hu</a></dd>
-				<dd class="clear"></dd>
-
-				<dt>Web:</dt>
-				<dd>
-				    <a href="http://adobi.hu" class = "web url">adobi.hu</a>, 
-				    <a href="http://www.linkedin.com/in/adobi" target = "_blank">LinkedIn</a>, 
-				    <a href="http://adobi.github.com/" target = "_blank">GitHub</a>, 
-				    <a href="http://facebook.com/adobi" target = "_blank">Facebook</a>, 
-				    <a href="http://twitter.com/adobi" target = "_blank">Twitter</a>
-			    </dd>
-				<dd class="clear"></dd>
-			</dl>
-			
+			<h1 class = "fn">
+				Attila Dobi
+				<div class="adobi-img"></div>
+			</h1>
 		</div> <!-- contact-info -->
 		
 		<div class = "clear"></div>
 		
 		<div id="personal-info">
 			<dl>
+				<dt>
+					Personal info
+				</dt>
+				<dd>
+					<dl class="personal-details">
+						<dt>Birth date</dt>
+						<dd class = "tel">09.10.1982</dd>
+						<dd class="clear"></dd>
+						<dt>Family status</dt>
+						<dd class = "tel">Married</dd>
+						<dd class="clear"></dd>
+						<dt>Cell</dt>
+						<dd class = "tel"><div>+36-70-39-27-700</div> <div style="margin-top:10px;">+40-74-02-98-580</div></dd>
+						<dd class="clear"></dd>
+						<dt>Email</dt>
+						<dd><a href="mailto:hello@adobi.hu" class = "email">hello[at]adobi.hu</a></dd>
+						<dd class="clear"></dd>
+						<dt>Web</dt>
+						<dd>
+					    <a href="http://adobi.hu" class = "web url">adobi.hu</a>, 
+					    <a href="http://www.linkedin.com/in/adobi" target = "_blank">LinkedIn</a>, 
+					    <a href="http://adobi.github.com/" target = "_blank">GitHub</a>, 
+					    <a href="http://facebook.com/adobi" target = "_blank">Facebook</a>, 
+					    <a href="http://twitter.com/adobi" target = "_blank">Twitter</a>
+				    </dd>
+						<dd class="clear"></dd>
+					</dl>
+				</dd>
+				<dd class="clear"></dd>
 				<dd class="clear"></dd>
 
 				<dt>Experience</dt>
 				<dd>
 					<h2>wizando.com <a href="http://wizando.com" target="_blank">&raquo;</a><span>2012 october-present</span></h2>
 					<p>Front-End Engineer.</p>
-					<p>I'm woking on the front-end of the application, designing and implementing mobile and user friendly solutions.</p>
-					<p>Technology stack: HTML5, CSS3(with <a href="http://lesscss.org" target="_blank">Less</a>), 
+					<p>My responsability is to design, build and maintain the front-end of the application.</p>
+					<p>Technology stack: HTML5, CSS3(with <a href="http://lesscss.org" target="_blank">Less</a>, responsive layout), 
 						jQuery, 
 						<a href="http://gruntjs.com" target="_blank">Grunt</a>, 
 						<a href="http://getbootstrap.com" target="_blank">Twitter's Bootstrap</a>, 
@@ -64,29 +58,29 @@
 					</p>
 					<h2>Invictus-Games Ltd.<a href="http://invictus.com" target="_blank">&raquo;</a><span>2010 july-2012 october</span></h2>
 					<p>Full-stack web developer.</p>
-					<p>My job was to design and implement several web-based applications.</p>
-					<p style="margin-bottom:0">Some of them:</p>
+					<p style="margin-bottom:0">My job was to design and implement several web applications. Some of them:</p>
 					<ul style="margin-bottom:10px;">
-						<li><a href="http://invictus.com">invictus.com</a>: Company website. My job was the design and implementation of a custom CMS. <div>Technology stack: CodeIgniter, Twitter's Bootstrap, Less, Grunt</div></li>
-            <li><a href="http://press.invictus.com">press.invictus.hu</a>: A CMS for press releases</li>
-						<li><a href="http://innologin.hu">innologin.hu</a>: website development.</li>
-						<li><a href="http://igenx.hu">igenx.hu</a>: application design, contact with the customer.<div>Technology stack: CodegnIter, jQuery, CSS, HTML</div></li>
+						<li><a href="http://invictus.com">invictus.com</a>: Company website. Role: design and implementation of a custom CMS. <div>Technology stack: CodeIgniter, Twitter's Bootstrap, Less, Grunt, MySQL.</div></li>
+            <li><a href="http://press.invictus.com">press.invictus.hu</a>: A CMS for press releases. Role: design and implementation. <div>Technology stack: CodegnIter, jQuery, CSS, HTML, MySQL.</div></li>
+						<li><a href="http://innologin.hu">innologin.hu</a>: Company website. Role: development.</li>
+						<li><a href="http://igenx.hu">igenx.hu</a>: Web application. Role: application design, contact with the customer, developement.<div>Technology stack: CodegnIter, jQuery, CSS, HTML, MySQL.</div></li>
 					</ul>
           <p style="margin-bottom:0;">Other, internally used tools and systems:</p>
           <ul>
             <li>Several API's for social and mobile games.</li>
             <li>A data-driven cross-promotion system for mobile games.</li>
-            <li>Several application to help to publish and promote games: a CMS for press releases, for microsites, in-game news</li>
+            <li>Several application to help to publish and promote games: a CMS for microsites, in-game news</li>
+            <li>A social game(un-published). Role: development of an API for the flash client and the CodeIgniter backend with a MongoDB database.</li>
           </ul>
 
 					<h2>Web-Server Ltd.<a href="http://web-server.hu" target="_blank">&raquo;</a><span>2008 december-2010 june</span></h2>
 					<p>Full-stack web developer.</p>
-          <p style="margin-bottom:0">My job was to design and implement several internally used applications, such as:</p>
+          <p style="margin-bottom:0">My job was to design and implement several internally used web applications, such as:</p>
           <ul style="margin-bottom:0;">
             <li>An error reporting application, to help the work and communication between a front and back office.</li>
-            <li>A server management application</li>
+            <li>A server management application.</li>
           </ul>
-          <p>Technology stack: ZendFramework, jQuery</p>
+          <p>Technology stack: ZendFramework, HTML, jQuery, CSS, MySQL</p>
 					
 					<h2>Freelancer <span>2006-present</span></h2>
 					<p>Developing websites and web applications for clients from Romania and USA.</p>
@@ -133,16 +127,12 @@
 					
 					<h2>Databases</h2>
 					<p>
-						MySQL,
-						Oracle,
-						SQLite,
-						MongoDB
+						MySQL, Oracle, SQLite, MongoDB
 					</p>
 					
 					<h2>Operating systems</h2>
 					<p>
-						Windows,
-						Linux
+						Windows, Linux
 					</p>
 					
 					<h2>Server applications</h2>
@@ -171,22 +161,23 @@
 				</dd>
 				<dd class = "clear"></dd>
 				
-				<dt>Projects</dt>
+				<dt>Side projects</dt>
 				<dd>
-					<h2>
-					    Some of my hobby projects					    
-			    </h2> 
           <ul>
-            <li><a href="http://dent.doctype.hu" target="_blank">Simple-Dent</a> A management system for dentists(orthodontists)</li>
-            <li><a href="http://color.adobi.hu" target="_blank">color.adobi.hu</a> a collection of colors for future inspiration</li>
-            <li><a href="http://adobi.github.io/" target="_blank">Some of my github repos</a></li>
+            <li><a href="http://dent.doctype.hu" target="_blank">Simple-Dent</a> A management application for dentists(orthodontists).</li>
+            <li><a href="http://color.adobi.hu" target="_blank">color.adobi.hu</a> a collection of colors for future inspiration.</li>
+            <li><a href="http://adobi.github.io/" target="_blank">Github repositories</a></li>
           </ul>
 				</dd>
 				<dd class = "clear"></dd>
 
 				<dt>Language skills</dt>
 				<dd>
-					Hungarian, English, Romanian
+					<ul>
+						<li>Hungarian <i>(mother tongue)</i></li>
+						<li>English <i>(intermediate)</i></li>
+						<li>Romanian <i>(intermediate)</i></li>
+					</ul>
 				</dd>
 				<dd class = "clear"></dd>
 				
