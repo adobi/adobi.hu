9d23a5d62f93247912a4306dd7dff30c2db0cf34
Attila Dobi <dobiattila@gmail.com>
2015-08-04 11:57:25 +0300

    Index page design changes

--- lumen/adobi.hu/resources/views/index.php
+++ lumen/adobi.hu/resources/views/index.php
@@ -4,11 +4,20 @@
     <div class="site-wrapper-inner">
         <div class="cover-container">
             <div class="inner cover">
-                <h1 class="cover-heading">Hello!</h1>
-                <p class="lead">I'm Attila Dobi - <a href="<?php echo url('resume') ?>">adobi</a>, a full-stack web developer. </p>
-                <p class="lead">
-                    <a href="<?php echo url('resume') ?>" class="">Check out my Resume</a>
-                </p>
+                <h1 class="cover-heading uppercase">Hell0!</h1>
+                <div class="lead">
+                    I'm
+                    <a href="<?php echo url('resume') ?>"><span class="uppercase">Attila Dobi</span></a>
+                    &mdash;
+                    <a href="https://twitter.com/adobi">@adobi</a>
+                </div>
+                <div class="lead">
+                    Full-stack web developer.
+                </div>
+                <div class="lead sub-lead">
+                    <a href="<?php echo url('resume') ?>">Resume</a>
+                    <a href="mailto:hello[at]adobi.hu">Contact</a>
+                </div>
             </div>
             <div class="mastfoot">
                 <div class="inner">
