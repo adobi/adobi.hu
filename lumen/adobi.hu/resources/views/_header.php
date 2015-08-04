69ab13a38d70a13635353e67b7bb5ddcfc5fc207
Attila Dobi <dobiattila@gmail.com>
2015-08-04 11:30:55 +0300

    Lumen initial setup and migration

---
+++ lumen/adobi.hu/resources/views/_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Attila Dobi - adobi</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="I'm Dobi Attila, a full-stack web developer." />
+        <meta name="keywords" content="Dobi Attila, web developer, web development, php, javascript" />
+        <meta name="author" content="Dobi Attila" />
+        <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+        <meta name="google-site-verification" content="7xn52P4_OQD2-me0rq4p86dvEgUhxzggnwcIAnd5xX0" />
+
+        <link rel="stylesheet" href="<?= url('css/bootstrap.min.css') ?>">
+        <link rel="stylesheet" href="<?= url('css/bootstrap-theme.min.css') ?>">
+        <link rel="stylesheet" href="<?= url('css/page.css') ?>">
+
+        <link href='//fonts.googleapis.com/css?family=Lato:100,300,400' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
---
+++ lumen/adobi.hu/resources/views/index.php
@@ -0,0 +1,21 @@
+<?php require_once '_header.php' ?>
+
+<div class="site-wrapper">
+    <div class="site-wrapper-inner">
+        <div class="cover-container">
+            <div class="inner cover">
+                <h1 class="cover-heading">Hello!</h1>
+                <p class="lead">I'm Attila Dobi - <a href="<?php echo url('resume') ?>">adobi</a>, a full-stack web developer. </p>
+                <p class="lead">
+                    <a href="<?php echo url('resume') ?>" class="">Check out my Resume</a>
+                </p>
+            </div>
+            <div class="mastfoot">
+                <div class="inner">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once '_footer.php' ?>
