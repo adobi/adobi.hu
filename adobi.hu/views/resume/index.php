c4081b1755920b397eda4f0eac597e464b7db8ee
Attila Dobi <dobiattila@gmail.com>
2015-08-04 08:09:06 +0300

    update resume

--- adobi.hu/views/resume/index.php
+++ adobi.hu/views/resume/index.php
@@ -1,197 +1,258 @@
+<div id="resume-wrap">
 
-	<div id="resume-wrap">
-		
-		<div id="contact-info" class = "vcard">
-			<p id="download-resume" style="margin-bottom:30px;text-align:center;"><a href="<?php base_url() ?>docs/adobi-resume.pdf">download resume as pdf</a></p>
-			<h1 class = "fn">
-				<div class="adobi-img"></div>
-				Attila Dobi <span class="download"></span>
-			</h1>
-		</div> <!-- contact-info -->
-		
-		<div class = "clear"></div>
-		
-		<div id="personal-info">
-			<dl>
-				<dt>
-					Personal info
-				</dt>
-				<dd>
-					<dl class="personal-details">
-						<dt>Birth date</dt>
-						<dd class = "tel">09.10.1982</dd>
-						<dd class="clear"></dd>
-						<dt>Family status</dt>
-						<dd class = "tel">Married</dd>
-						<dd class="clear"></dd>
-						<dt>Cell</dt>
-						<dd class = "tel"><div style="margin-bottom:10px;">+40 74 02 98 580 <span>(primary)</span></div><div>+36 70 39 27 700 <span>(not always turned on)</span></div> </dd>
-						<dd class="clear"></dd>
-						<dt>Email</dt>
-						<dd><a href="mailto:hello@adobi.hu" class = "email">hello[at]adobi.hu</a></dd>
-						<dd class="clear"></dd>
-						<dt>Skype</dt>
-						<dd><a href="#" class = "email">adobi82</a></dd>
-						<dd class="clear"></dd>
-						<dt>Web</dt>
-						<dd>
-					    <a href="http://adobi.hu" class = "web url">adobi.hu</a>, 
-					    <a href="http://www.linkedin.com/in/adobi" target = "_blank">LinkedIn</a>, 
-					    <a href="http://github.com/adobi" target = "_blank">GitHub</a>, 
-					    <a href="http://facebook.com/adobi" target = "_blank">Facebook</a>, 
-					    <a href="http://twitter.com/adobi" target = "_blank">Twitter</a>
-				    </dd>
-						<dd class="clear"></dd>
-					</dl>
-				</dd>
-				<dd class="clear"></dd>
-				<dd class="clear"></dd>
+    <div id="contact-info" class="vcard">
+        <p id="download-resume" style="margin-bottom:30px;text-align:center;">
+            <a href="<?php base_url() ?>docs/adobi-resume.pdf">download resume as pdf</a></p>
 
-				<dt>Experience</dt>
-				<dd class="experience">
-					<h2><span></span> wizando.com <a href="http://wizando.com" target="_blank">&raquo;</a><span class="duration">2012 october-present</span></h2>
-					<p class="position">Front-End Engineer.</p>
-					<div class="details">
-						<p>My responsability is to design, build and maintain the front-end of the application. In the past few months I also worked with the back-end(Symfony).</p>
-						<p>Technology stack: <a href = "http://symfony.com/" target="_blank">Symfony 2.3</a>, HTML5, CSS3(with <a href="http://lesscss.org" target="_blank">Less</a>, responsive layout), 
-							jQuery, 
-							<a href="http://gruntjs.com" target="_blank">Grunt</a>, 
-							<a href="http://getbootstrap.com" target="_blank">Twitter's Bootstrap</a>, 
-							<a href="http://twig.sensiolabs.org/" target="_blank">Twig</a>,
-							Git
-						</p>
-					</div>	
+        <h1 class="fn">
+            <div class="adobi-img"></div>
+            Attila Dobi <span class="download"></span>
+        </h1>
+    </div>
+    <!-- contact-info -->
 
-					<h2><span></span> Invictus-Games Ltd.<a href="http://invictus.com" target="_blank">&raquo;</a><span class="duration">2010 july-2012 october</span></h2>
-					<p class="position">Full-stack web developer.</p>
-					<div class="details">
-						<p style="margin-bottom:0">My job was to design and implement several web applications. Some of them:</p>
-						<ul style="margin-bottom:10px;">
-							<li><a href="http://invictus.com">invictus.com</a>: Company website. Role: design and implementation of a custom CMS. <div>Technology stack: CodeIgniter, Twitter's Bootstrap, Less, Grunt, MySQL.</div></li>
-	            <li><a href="http://press.invictus.com">press.invictus.hu</a>: A CMS for press releases. Role: design and implementation. <div>Technology stack: CodegnIter, jQuery, CSS, HTML, MySQL.</div></li>
-							<li><a href="http://innologin.hu">innologin.hu</a>: Company website. Role: development.</li>
-							<li><a href="http://igenx.hu">igenx.hu</a>: Web application. Role: application design, contact with customers, development.<div>Technology stack: CodegnIter, jQuery, CSS, HTML, MySQL.</div></li>
-						</ul>
-	          <p style="margin-bottom:0;">Other, internally used tools and systems:</p>
-	          <ul>
-	            <li>Several API's for social and mobile games.</li>
-	            <li>A data-driven cross-promotion system for mobile games.</li>
-	            <li>Several applications to help publishing and promoting games: a CMS for microsites, in-game news.</li>
-	            <li>A social game(un-published). Role: development of an API for the client, a HTML and Javascript front-end and a back-end using CodeIgniter and MongoDB.</li>
-	          </ul>
-					</div>	
+    <div class="clear"></div>
 
-					<h2><span></span> Web-Server Ltd.<a href="http://web-server.hu" target="_blank">&raquo;</a><span class="duration">2008 december-2010 june</span></h2>
-					<p class="position">Full-stack web developer.</p>
-					<div class="details">
-	          <p style="margin-bottom:0">My job was to design and implement several internally used web applications, such as:</p>
-	          <ul style="margin-bottom:0;">
-	            <li>An error reporting application, to help the work and communication between a front and back office.</li>
-	            <li>A server management tool.</li>
-	          </ul>
-	          <p>Technology stack: ZendFramework, HTML, jQuery, CSS, MySQL</p>
-					</div>
+    <div id="personal-info">
+        <dl>
+            <dt>
+                Personal info
+            </dt>
+            <dd>
+                <dl class="personal-details">
+                    <dt>Birth date</dt>
+                    <dd class="tel">09.10.1982</dd>
+                    <dd class="clear"></dd>
+                    <dt>Family status</dt>
+                    <dd class="tel">Married</dd>
+                    <dd class="clear"></dd>
+                    <dt>Cell</dt>
+                    <dd class="tel">
+                        <div style="margin-bottom:10px;">+40 74 02 98 580 <span>(primary)</span></div>
+                        <div>+36 70 39 27 700 <span>(not always turned on)</span></div>
+                    </dd>
+                    <dd class="clear"></dd>
+                    <dt>Email</dt>
+                    <dd><a href="mailto:hello@adobi.hu" class="email">hello[at]adobi.hu</a></dd>
+                    <dd class="clear"></dd>
+                    <dt>Skype</dt>
+                    <dd><a href="#" class="email">adobi82</a></dd>
+                    <dd class="clear"></dd>
+                    <dt>Web</dt>
+                    <dd>
+                        <a href="http://adobi.hu" class="web url">adobi.hu</a>,
+                        <a href="http://www.linkedin.com/in/adobi" target="_blank">LinkedIn</a>,
+                        <a href="http://github.com/adobi" target="_blank">GitHub</a>,
+                        <a href="http://facebook.com/adobi" target="_blank">Facebook</a>,
+                        <a href="http://twitter.com/adobi" target="_blank">Twitter</a>
+                    </dd>
+                    <dd class="clear"></dd>
+                </dl>
+            </dd>
+            <dd class="clear"></dd>
+            <dd class="clear"></dd>
 
-					<h2><span></span> Freelancer <span class="duration">2006-present</span></h2>
-					<div class="details">
-						<p style="margin-bottom:0">Developing websites and web applications for different clients.</p>
-						<ul>
-							<li><a href="http://www.hotellycium.hu/" target="_blank">hotellycium.hu</a></li>
-	            <li><a href="http://est-vest.ro/" target="_blank">est-vest.ro</a></li>
-						</ul>
-					</div>
-				</dd>
-				<dd class = "clear"></dd>
-				<dt>Side projects</dt>
-				<dd>
-          <ul>
-            <li><a href="http://dent.doctype.hu" target="_blank">Simple-Dent</a> A management application for dentists(orthodontists).</li>
-            <li><a href="http://adobi.hu/" target="_blank">adobi.hu</a> My personal website</li>
-            <li><a href="http://kassayzsuzsa.com/" target="_blank">kassayzsuzsa.com</a> My wife's website</li>
-            <li><a href="http://color.adobi.hu" target="_blank">color.adobi.hu</a> A collection of colors for future inspiration.</li>
-            <li><a href="http://adobi.github.io/" target="_blank">Github repositories</a></li>
-          </ul>
-				</dd>
-				<dd class = "clear"></dd>
-		
-				<dt>Education</dt>
-				<dd>
-					<h2>University of Debrecen Faculty of Informatics</h2>
-					<p>
-						Bachelor's Degree in Software Engineering and Mathematics
-						<small>Graduated in: June 2008</small>
-					</p>
-					
-				</dd>
-				<dd class="clear"></dd>
-				
-				<dt>Skills</dt>
-				<dd class="skills">
-					<h2>Programming languages</h2>
-					<p>
-						PHP <span>(
-                <a href = "http://laravel.com/" target="_blank">Laravel</a>,
-                <a href = "http://symfony.com/" target="_blank">Symfony</a>,
-						    <a href = "http://codeigniter.com" target="_blank">CodeIgniter</a>,
-						    <a href = "http://www.yiiframework.com" target="_blank">Yii</a>,
-                <a href = "http://framework.zend.com" target="_blank">Zend Framework</a>
+            <dt>Experience</dt>
+            <dd class="experience">
+                <h2>
+                    Webapix Interactive
+                    <a href="http://webapix.hu" target="_blank">&raquo;</a><span class="duration">2015 april-present</span>
+                </h2>
+
+                <p class="position">Full-stack web developer</p>
+
+                <div class="details">
+                    <p>
+                        My responsibility is to maintain and develop new feature for sites like:
+                    </p>
+                    <ul>
+                        <li><a href="http://dibook.hu" target="_blank">dibook.hu</a></li>
+                        <li><a href="http://dimag.hu" target="_blank">dimag.hu</a></li>
+                        <li><a href="http://ibusz.hu" target="_blank">ibusz.hu</a></li>
+                    </ul>
+                </div>
+                <h2>
+                    wizando.com
+                    <a href="http://wizando.com" target="_blank">&raquo;</a><span class="duration">2012 october-2015 april</span>
+                </h2>
+
+                <p class="position">Front-End Engineer</p>
+
+                <div class="details">
+                    <p>My responsibility is to design, build and maintain the front-end of the application. In the past few months I also worked with the back-end(Symfony).</p>
+
+                    <p>Technology stack: <a href="http://symfony.com/" target="_blank">Symfony 2.3</a>, HTML5, CSS3(with
+                        <a href="http://lesscss.org" target="_blank">Less</a>, responsive layout),
+                        jQuery,
+                        <a href="http://gruntjs.com" target="_blank">Grunt</a>,
+                        <a href="http://getbootstrap.com" target="_blank">Twitter's Bootstrap</a>,
+                        <a href="http://twig.sensiolabs.org/" target="_blank">Twig</a>,
+                        Git
+                    </p>
+                </div>
+
+                <h2>
+                    Invictus-Games Ltd.<a href="http://invictus.com" target="_blank">&raquo;</a><span class="duration">2010 july-2012 october</span>
+                </h2>
+
+                <p class="position">Full-stack web developer</p>
+
+                <div class="details">
+                    <p style="margin-bottom:0">My job was to design and implement several web applications. Some of them:</p>
+                    <ul style="margin-bottom:10px;">
+                        <li>
+                            <a href="http://invictus.com">invictus.com</a>: Company website. Role: design and implementation of a custom CMS.
+                            <div>Technology stack: CodeIgniter, Twitter's Bootstrap, Less, Grunt, MySQL.</div>
+                        </li>
+                        <li>
+                            <a href="http://press.invictus.com">press.invictus.hu</a>: A CMS for press releases. Role: design and implementation.
+                            <div>Technology stack: CodegnIter, jQuery, CSS, HTML, MySQL.</div>
+                        </li>
+                        <li><a href="http://innologin.hu">innologin.hu</a>: Company website. Role: development.</li>
+                        <li>
+                            <a href="http://igenx.hu">igenx.hu</a>: Web application. Role: application design, contact with customers, development.
+                            <div>Technology stack: CodegnIter, jQuery, CSS, HTML, MySQL.</div>
+                        </li>
+                    </ul>
+                    <p style="margin-bottom:0;">Other, internally used tools and systems:</p>
+                    <ul>
+                        <li>Several API's for social and mobile games.</li>
+                        <li>A data-driven cross-promotion system for mobile games.</li>
+                        <li>Several applications to help publishing and promoting games: a CMS for microsites, in-game news.</li>
+                        <li>A social game(un-published). Role: development of an API for the client, a HTML and Javascript front-end and a back-end using CodeIgniter and MongoDB.</li>
+                    </ul>
+                </div>
+
+                <h2>
+                    Web-Server Ltd.<a href="http://web-server.hu" target="_blank">&raquo;</a><span class="duration">2008 december-2010 june</span>
+                </h2>
+
+                <p class="position">Full-stack web developer</p>
+
+                <div class="details">
+                    <p style="margin-bottom:0">My job was to design and implement several internally used web applications, such as:</p>
+                    <ul style="margin-bottom:0;">
+                        <li>An error reporting application, to help the work and communication between a front and back office.</li>
+                        <li>A server management tool.</li>
+                    </ul>
+                    <p>Technology stack: ZendFramework, HTML, jQuery, CSS, MySQL</p>
+                </div>
+
+                <h2><span></span> Freelancer <span class="duration">2006-present</span></h2>
+
+                <div class="details">
+                    <p style="margin-bottom:0">Developing websites and web applications for different clients.</p>
+                    <ul>
+                        <li><a href="http://www.hotellycium.hu/" target="_blank">hotellycium.hu</a></li>
+                        <li><a href="http://est-vest.ro/" target="_blank">est-vest.ro</a></li>
+                    </ul>
+                </div>
+            </dd>
+            <dd class="clear"></dd>
+            <dt>Side projects</dt>
+            <dd>
+                <ul>
+                    <li>
+                        <a href="http://dent.doctype.hu" target="_blank">Simple-Dent</a> A management application for dentists(orthodontists).
+                    </li>
+                    <li><a href="http://adobi.hu/" target="_blank">adobi.hu</a> My personal website</li>
+                    <li><a href="http://kassayzsuzsa.com/" target="_blank">kassayzsuzsa.com</a> My wife's website</li>
+                    <li>
+                        <a href="http://color.adobi.hu" target="_blank">color.adobi.hu</a> A collection of colors for future inspiration.
+                    </li>
+                    <li><a href="http://adobi.github.io/" target="_blank">Github repositories</a></li>
+                </ul>
+            </dd>
+            <dd class="clear"></dd>
+
+            <dt>Education</dt>
+            <dd>
+                <h2>University of Debrecen Faculty of Informatics</h2>
+
+                <p>
+                    Bachelor's Degree in Software Engineering and Mathematics
+                    <small>Graduated in: June 2008</small>
+                </p>
+
+            </dd>
+            <dd class="clear"></dd>
+
+            <dt>Skills</dt>
+            <dd class="skills">
+                <h2>Programming languages</h2>
+
+                <p>
+                    PHP <span>(
+                <a href="http://laravel.com/" target="_blank">Laravel</a>,
+                <a href="http://symfony.com/" target="_blank">Symfony</a>,
+						    <a href="http://codeigniter.com" target="_blank">CodeIgniter</a>,
+						    <a href="http://www.yiiframework.com" target="_blank">Yii</a>,
+                <a href="http://framework.zend.com" target="_blank">Zend Framework</a>
 						)</span>,
-						Javascript <span>(<a href = "http://jquery.com" target="_blank">jQuery</a>)</span>,
-						Java(J2SE),
-						C
-					</p>
-					
-					<h2>Markup languages</h2>
-					<p>
-						HTML(5), CSS<span>(2, 3)</span>, XML, JSON
-					</p>
-					
-					<h2>Databases</h2>
-					<p>
-						MySQL, SQLite, MongoDB, Oracle
-					</p>
-					
-					<h2>Version control</h2>
-					<p>
-						GIT (Github), SVN, CVS
-					</p>
-					
-					<h2>Operating systems</h2>
-					<p>
-						Windows, Linux
-					</p>
-					
-					<h2>Testing</h2>
-					<p>
-						PHPUnit
-					</p>
+                    Javascript <span>(<a href="http://jquery.com" target="_blank">jQuery</a>)</span>,
+                    Java(J2SE),
+                    C
+                </p>
 
-					<h2>Design</h2>
-					<p>UML, Design Patterns, UI/UX, Responsive design</p>
+                <h2>Markup languages</h2>
 
-					<h2>API's</h2>
-					<p>Facebook, Twitter</p>
+                <p>
+                    HTML(5), CSS<span>(2, 3)</span>, XML, JSON
+                </p>
 
-          <h2>Analytical tools</h2>
-          <p>Google Analytics</p>
-				</dd>
-				<dd class = "clear"></dd>
-				
-				<dt>Language skills</dt>
-				<dd class="lang">
-					<ul>
-						<li>Hungarian <span>(mother tongue)</span></li>
-						<li>English <span>(intermediate)</span></li>
-						<li>Romanian <span>(intermediate)</span></li>
-					</ul>
-				</dd>
-				<dd class = "clear"></dd>
-				
-				<dt>Others</dt>
-				<dd>Category B driving license</dd>
-				<dd class="clear"></dd>
-			</dl>
+                <h2>Databases</h2>
 
-		</div>
-				
-	</div> <!-- resume-wrap -->
+                <p>
+                    MySQL, SQLite, MongoDB, Oracle
+                </p>
+
+                <h2>Version control</h2>
+
+                <p>
+                    GIT (Github), SVN, CVS
+                </p>
+
+                <h2>Operating systems</h2>
+
+                <p>
+                    Windows, Linux
+                </p>
+
+                <h2>Testing</h2>
+
+                <p>
+                    PHPUnit
+                </p>
+
+                <h2>Design</h2>
+
+                <p>UML, Design Patterns, UI/UX, Responsive design</p>
+
+                <h2>API's</h2>
+
+                <p>Facebook, Twitter</p>
+
+                <h2>Analytical tools</h2>
+
+                <p>Google Analytics</p>
+            </dd>
+            <dd class="clear"></dd>
+
+            <dt>Language skills</dt>
+            <dd class="lang">
+                <ul>
+                    <li>Hungarian <span>(mother tongue)</span></li>
+                    <li>English <span>(intermediate)</span></li>
+                    <li>Romanian <span>(intermediate)</span></li>
+                </ul>
+            </dd>
+            <dd class="clear"></dd>
+
+            <dt>Others</dt>
+            <dd>Category B driving license</dd>
+            <dd class="clear"></dd>
+        </dl>
+
+    </div>
+
+</div> <!-- resume-wrap -->
